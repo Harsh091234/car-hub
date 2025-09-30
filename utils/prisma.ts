a9796aa214450ebb019f8ced8038bf4c141a14b1
Harsh091234 <hsharma4090@gmai.com>
2025-09-30 23:40:57 +0530

    dfsa

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
